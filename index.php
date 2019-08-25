ed35fe0196d2f68fbce60319cc7bb9d8e45c716c
griffinstiens <griffinstiens@gmail.com>
2019-08-25 18:14:18 -0500

    test

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
+    <style>
+      body {
+        background-color: #757575;
+      }
+
+      h2 {
+        font-family: 'Lobster', cursive;
+      }
+    </style>
+    <title>CS4830</title>
+  </head>
+  <body>
+    <div class="container"
+      <div class="row">
+        <div class="col s12 m8 l6">
+            <div class="card-panel grey lighten-5 z-depth-1">
+              <div class="row valign-wrapper">
+                <div class="col s2">
+                  <img src="images/griffin.jpeg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                </div>
+                <div class="col s10">
+                  <h2 class="black-text">Griffin C. Stiens</h2><br>
+                  <span class="black-text">
+                    This will serve as the hub for all projects completed in CS4830.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col s12 m4 l4">
+            <div class="card-panel grey lighten-5 z-depth-1">
+              <div class="row valign-wrapper">
+                <div class="col s12">Challenge #1</div>
+              </div>
+
+            </div>
+          </div>
+
+      </div>
+    </div>
+  </body>
+</html>
