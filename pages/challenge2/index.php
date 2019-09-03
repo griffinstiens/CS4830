d1e9dfab1ba09e5b0fcfdd4d7cb3227b47bcdb6f
griffinstiens <griffinstiens@gmail.com>
2019-09-03 14:37:09 -0500

    adding custom error pages

---
+++ pages/challenge2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>Image Uploader</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col s12">
+          <h1 class="center">Challenge 2</h1>
+          <p class="flow-text">Here is the second challenge. Click the button below to upload an image! After you receive
+          a success message, go to the 'Results' page to see your image.</p>
+        </div>
+
+      </div>
+    </div>
+  </body>
+</html>
