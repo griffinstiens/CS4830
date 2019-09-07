1081557873a27f28bc9896f351441c74bf10493b
griffinstiens <griffinstiens@gmail.com>
2019-09-07 14:49:02 -0500

    adding custom error pages

--- pages/challenge2/scripts/upload.php
+++ pages/challenge2/scripts/upload.php
@@ -11,9 +11,9 @@ if(isset($_POST["submit"])){
 
         //DB details
         $dbHost     = 'localhost';
-        $dbUsername = 'root';
-        $dbPassword = '*****';
-        $dbName     = 'codexworld';
+        $dbUsername = 'griffin';
+        $dbPassword = 'test'
+        $dbName     = 'CS4830';
 
         //Create connection and select DB
         $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
