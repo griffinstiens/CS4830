7b12f5702a85eed274a5a3247c09e1d232b6ba69
griffinstiens <griffinstiens@gmail.com>
2019-09-07 11:27:13 -0500

    adding custom error pages

--- pages/challenge2/db/config.php
+++ pages/challenge2/db/config.php
@@ -1,6 +1,6 @@
 <?php
   DEFINE("HOST", "localhost"); //localhost on server
   DEFINE("DBNAME", "CS4830");
-  DEFINE("USERNAME", "root");
-  DEFINE("PASSWORD", "tiddies");
+  DEFINE("USERNAME", "griffin");
+  DEFINE("PASSWORD", "test");
  ?>
