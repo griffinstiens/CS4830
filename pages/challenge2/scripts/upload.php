aadba50179e2775c2c9f644b9283270476f5a28b
griffinstiens <griffinstiens@gmail.com>
2019-09-07 12:34:01 -0500

    adding custom error pages

--- pages/challenge2/scripts/upload.php
+++ pages/challenge2/scripts/upload.php
@@ -10,36 +10,49 @@ if (isset($_POST['submit'])) {
   $file_size = $_FILES['file']['size'];
   $file_error = $_FILES['file']['error'];
   $file_data = time();
+  $file_path = '../uploads';
 
-  $file_Ext = explode('.', $file_name);
-  $file_Actual_Ext = strtolower(end($file_Ext));
+  move_uploaded_file($fileTmpName, $file_path.'/');
+  $sql = "INSERT INTO images (file_path, file_size, file_type, file_data) VALUES ('$target_destination', '$file_size', '$file_type','$file_data')";
 
-  $allow = array('jpg','jpeg','png');
-
-  if (in_array($file_Actual_Ext, $allow)) {
-    if ($file_error === 0) {
-      if ($file_size < 1000000) {
-        $new_file_name = uniqid('', true).".".$file_Actual_Ext;
-        $target_destination = '../uploads/'.$new_file_name;
-
-        $sql = "INSERT INTO images (file_path, file_size, file_type, file_data) VALUES ('$target_destination', '$file_size', '$file_type','$file_data')";
-        $stmt = $conn->stmt_init();
-        $stmt->prepare($sql);
-        $stmt->bind_param('sis', $file_path, $file_size, $file_type);
-        $stmt->execute();
-
-        header("Location: ../index.php?uploadSuccess");
-        echo "Success";
-      } else {
-        echo "Your file is too big";
-      }
-    } else {
-      echo "There was an error uploading your image.";
-    }
-
+  if(mysqli_query($sql)) {
+    echo "Success!";
   } else {
-    echo "Unsupported file type. Please only upload .jpg,.jpeg,.png";
+    echo "Fuck pls work";
   }
+
+
+
+
+
+  //
+  // $file_Ext = explode('.', $file_name);
+  // $file_Actual_Ext = strtolower(end($file_Ext));
+  //
+  // $allow = array('jpg','jpeg','png');
+  //
+  // if (in_array($file_Actual_Ext, $allow)) {
+  //   if ($file_error === 0) {
+  //     if ($file_size < 1000000) {
+  //       $new_file_name = uniqid('', true).".".$file_Actual_Ext;
+  //       $target_destination = '../uploads/'.$new_file_name;
+  //       $sql = "INSERT INTO images (file_path, file_size, file_type, file_data) VALUES ('$target_destination', '$file_size', '$file_type','$file_data')";
+  //       if(mysql_query($sql)) {
+  //         move_uploaded_file($_FILES);
+  //       }
+  //
+  //       header("Location: ../index.php?uploadSuccess");
+  //       echo "Success";
+  //     } else {
+  //       echo "Your file is too big";
+  //     }
+  //   } else {
+  //     echo "There was an error uploading your image.";
+  //   }
+  //
+  // } else {
+  //   echo "Unsupported file type. Please only upload .jpg,.jpeg,.png";
+  // }
 
 } //end isset
 
