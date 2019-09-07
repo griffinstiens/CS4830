d4ea8b0ac99199a404ce2431ca200d28016ddfed
griffinstiens <griffinstiens@gmail.com>
2019-09-07 11:44:49 -0500

    adding custom error pages

--- pages/challenge2/scripts/upload.php
+++ pages/challenge2/scripts/upload.php
@@ -2,23 +2,68 @@
 require_once('../db/config.php');
 $conn = connectDB();
 
-extract($_POST);
+list($size, $width, $height, $type, $attr) = getimagesize("file");
+$file_size = filesize("file");
 
-$UploadedFileName=$_FILES['UploadImage']['name'];
-if($UploadedFileName!='')
-{
-  $upload_directory = "uploads/"; //This is the folder which you created just now
-  $TargetPath=time().$UploadedFileName;
-  if(move_uploaded_file($_FILES['files']['tmp_name'], $upload_directory.$TargetPath)){
-    $QueryInsertFile="INSERT INTO images SET ImageColumnName='$TargetPath'";
-    // Write Mysql Query Here to insert this $QueryInsertFile   .
-  }
+if ($file_size >= 1024) {
+  $file_size = number_format($file_size / 1024, 2) . ' KB';
+}
+elseif ($file_size >= 1048576) {
+  $file_size = number_format($file_size / 1048576, 2) . ' MB';
+}
+else {
+  $file_size = 0;
+}
+// echo "Size is : " . $bytes . "<br>";
+// echo "Image type :" . $type . "<br>";
+// echo "Image attribute :" .$attr;
+
+$file_path = 'uploads/'
+$type = $file_type;
+$file_date = time();
+$destination = "uploads/";
+$target_file = $destination . basename($_FILES["UploadFile"]["name"]);
+$uploadOK = 1;
+
+$imageType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["UploadFile"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($file_type != "jpg" && $file_type != "png" && $file_type != "jpeg"
+&& $file_type != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+
+if ($uploadOK === 0) {
+  echo "Image upload unsuccessful.";
+} else {
+
 }
 
 
-
-
 header("Location: ../index.php");
+
+
+
+
+
 
 function connectDB(){
   $conn = new mysqli(HOST, USERNAME, PASSWORD, DBNAME);
