93b1112ed121a6ee24122cf947719da134ef3b4a
griffinstiens <griffinstiens@gmail.com>
2019-09-07 17:07:59 -0500

    adding custom error pages

--- pages/challenge2/view.php
+++ pages/challenge2/view.php
@@ -1,19 +1,37 @@
 <?php
-  if(!empty($_GET['id'])){
       //DB details
       require_once('db/config.php');
       $conn = connectDB();
 
       //Get image data from database
-      $result = $db->mysqli_query("SELECT image_path FROM images WHERE id = {$_GET['id']}");
+      $query = mysqli_query($conn, "SELECT image_name, image_path FROM images");
 
-      if($result->row > 0){
-          $imgData = $result->fetch_assoc();
-          echo "<img src='".$row['image_path']."' />";
-      }else{
-          echo 'Image not found...';
+      while($rows = mysqli_fetch_array($query))
+      {
+        $image_name = $rows['image_name'];
+        $image_path = $rows['image_path'];
       }
-  }
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lexend+Zetta|Quicksand&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>View</title>
+  </head>
+  <body>
+    <div class="container">
+      <img src="<?php echo $image_path; ?>" alt="" title="<?php echo $image_name; ?>" width="300" height="200" class="responsive-img" />
+      <p><strong><?php echo $image_name; ?></strong></p>
+    </div>
+  </body>
+</html>
 
 
 
@@ -22,6 +40,11 @@
 
 
 
+
+
+
+
+<?php
   function connectDB(){
     $conn = new mysqli(HOST, USERNAME, PASSWORD, DBNAME);
     if ($conn->connect_error) {
