938206c4ec2806880ce2f2a20fb9e17608821a39
griffinstiens <griffinstiens@gmail.com>
2019-09-08 11:14:12 -0500

    adding custom error pages

--- pages/challenge2/view.php
+++ pages/challenge2/view.php
@@ -47,7 +47,7 @@
                 <img src="<?php echo $image_path; ?>" class="responsive-img" alt="missing image" />
               </div>
               <div class="card-content">
-                <i class="material-icons right">more_vert</i></span>
+                <span class="card-title activator grey-text text-darken-4">Image<i class="material-icons right">more_vert</i></span>
               </div>
               <div class="card-reveal">
                 <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
