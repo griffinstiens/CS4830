a8461c1492d1905cbc01cad512ccb7e8ae2ca761
griffinstiens <griffinstiens@gmail.com>
2019-09-08 11:32:09 -0500

    adding custom error pages

--- pages/challenge2/upload.php
+++ pages/challenge2/upload.php
@@ -16,6 +16,7 @@ if (isset($_POST['submit'])) {
   VALUES ('$image_name', '$image_path', '$image_type','$image_size')");
 
   if($sql) {
+    echo "Success! Redirecting...";
     header("Location: index.php");
   } else {
     echo "pls work";
