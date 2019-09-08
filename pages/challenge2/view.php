f9ebf2b08cf0631f5e2307e70ca50e0212c1d64c
griffinstiens <griffinstiens@gmail.com>
2019-09-08 11:16:24 -0500

    adding custom error pages

--- pages/challenge2/view.php
+++ pages/challenge2/view.php
@@ -48,7 +48,7 @@
               </div>
               <div class="card-content">
                 <span class="card-title activator grey-text text-darken-4">Image<i class="material-icons right">settings</i></span>
-                <p class="flow-text">Please select the cog wheel to the right to display photo information.</p>
+                <p>Please select the cog wheel to the right to display photo information.</p>
               </div>
               <div class="card-reveal">
                 <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
