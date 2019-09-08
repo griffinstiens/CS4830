07e37f078428c291b6f1628c772d4386fb25b8df
griffinstiens <griffinstiens@gmail.com>
2019-09-08 18:34:36 -0500

    test

--- pages/challenge2/index.php
+++ pages/challenge2/index.php
@@ -49,7 +49,7 @@ function connectDB(){
               <div class="card-content">
                 <p class="flow-text">Here is the second challenge. Click the button below to upload an image! After you receive
                 a success message, go to the 'View' page to see your image.</p>
-                <?php if(isset($_GET['success'])) echo $success ?>
+                <?php if(isset($_GET['success']))?><div class="success"><?php echo $_GET['success'];?></div>
                 <?php if(isset($_GET['error'])) echo $_GET['error'];?>
 
                 <form action="upload.php" method="post" enctype="multipart/form-data">
