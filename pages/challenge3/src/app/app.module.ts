adc82fe46d147ffb0ff268e77d5fac895d801f47
griffinstiens <griffinstiens@gmail.com>
2019-09-23 20:23:10 -0500

    push challenge3

---
+++ pages/challenge3/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    Title
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
