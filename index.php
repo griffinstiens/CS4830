10d499ff3f6956fd2699c3ef5bd2093b487e755c
griffinstiens <griffinstiens@gmail.com>
2019-10-02 23:03:21 -0500

    UI changes

--- index.php
+++ index.php
@@ -37,13 +37,21 @@
       <div class="container">
         <div class="row">
           <div class="col s12 m4 l4">
-            <h1>test</h1>
+            <h3 class="center">Who am I?</h3>
+            <p>My name is Griffin Stiens and I am an Information Technology senior at the University of Missouri - Columbia.
+              I have aspirations to become a talented front-end developer working with technologies like React or Angular.
+            </p>
           </div>
           <div class="col s12 m4 l4">
-            <h1>test</h1>
+            <h1 class="center">Why are you here?</h1>
+            <p>You are here because I most likely sent a resume and/or cover letter to you and listed my personal website.
+              The purpose of this is to give you a chance to get a live look at some of the projects I've worked on through my studies
+              and outside the classroom.
+            </p>
           </div>
           <div class="col s12 m4 l4">
-            <h1>test</h1>
+            <h1 class="center">Contact Me</h1>
+            <ul><li>(314)-265-4057</li><li>griffinstiens@gmail.com</li></ul>
           </div>
         </div>
       </div>
