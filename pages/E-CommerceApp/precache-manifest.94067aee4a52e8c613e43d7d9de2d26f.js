825928823339cc600c899ca3167061653e8ad959
griffinstiens <griffinstiens@gmail.com>
2019-10-06 15:20:45 -0500

    testing prod build

---
+++ pages/E-CommerceApp/precache-manifest.94067aee4a52e8c613e43d7d9de2d26f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687fa7db78e3d8417b3aaa8ede1a9a1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c31341ae888d32dbe253",
+    "url": "/static/css/main.9b4ccad4.chunk.css"
+  },
+  {
+    "revision": "63e4742a578f500ccb27",
+    "url": "/static/js/2.203b06d3.chunk.js"
+  },
+  {
+    "revision": "c31341ae888d32dbe253",
+    "url": "/static/js/main.cbbd91d0.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
