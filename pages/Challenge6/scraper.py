2df5bd1a17ff12d477f7cdcf63613bef770a1a3d
griffinstiens <griffinstiens@gmail.com>
2019-10-31 17:57:47 -0500

    scraper test

---
+++ pages/Challenge6/scraper.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class SpideyBoi(scrapy.Spider):
+    name = 'spidey_boi'
+    start_urls = ['https://brickset.com/sets/year-2019']
+
+    def parse(self, response):
+        SET_SELECTOR = '.set'
+        
+        for item in response.css(SET_SELECTOR):
+            NAME_SELECTOR = 'h1 ::text'
+            yield {
+                'name': item.css(NAME_SELECTOR).extract_first()
+            }
