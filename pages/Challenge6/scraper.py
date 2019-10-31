a7bb9ceeef990edb4c5a992ad6de1ab96e82888d
griffinstiens <griffinstiens@gmail.com>
2019-10-31 18:06:44 -0500

    scraper test

--- pages/Challenge6/scraper.py
+++ pages/Challenge6/scraper.py
@@ -6,9 +6,12 @@ class SpideyBoi(scrapy.Spider):
 
     def parse(self, response):
         SET_SELECTOR = '.set'
+        /*  */
+        PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
         
         for item in response.css(SET_SELECTOR):
             NAME_SELECTOR = 'h1 ::text'
             yield {
                 'name': item.css(NAME_SELECTOR).extract_first()
+                'pieces': item.css(PIECES_SELECTOR).extract_first()
             }
