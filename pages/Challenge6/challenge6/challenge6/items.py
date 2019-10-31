e2ba0a8c212bbb740b72bda4ca4bfc7864e999dc
griffinstiens <griffinstiens@gmail.com>
2019-10-31 18:49:38 -0500

    scraper test

--- pages/Challenge6/challenge6/challenge6/items.py
+++ pages/Challenge6/challenge6/challenge6/items.py
@@ -11,4 +11,7 @@ import scrapy
 class Challenge6Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    #pass
+    name = scrapy.Field()
+    pieces = scrapy.Field()
+    image = scrapy.Field()
--- pages/Challenge6/challenge6/challenge6/spiders/example.py
+++ pages/Challenge6/challenge6/challenge6/spiders/example.py
@@ -1,11 +1,23 @@
-# -*- coding: utf-8 -*-
 import scrapy
 
-
 class ExampleSpider(scrapy.Spider):
-    name = 'example'
-    allowed_domains = ['example.com']
-    start_urls = ['http://example.com/']
+   name = "brickset_spider"
+   start_urls = ['http://brickset.com/sets/year-2018']
 
-    def parse(self, response):
-        pass
+   def parse(self, response):
+      SET_SELECTOR = '.set'
+      for brickset in response.css(SET_SELECTOR): 
+         NAME_SELECTOR = "h1 ::text"
+         PIECES_SELECTOR = ".//dl[dt/text() = 'Pieces']/dd/a/text()"
+         IMAGE_SELECTOR = "img ::attr(src)" 
+
+         yield {
+            'name': brickset.css(NAME_SELECTOR).extract_first(),
+            'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
+            'image': brickset.css(IMAGE_SELECTOR).extract_first()
+         }
+
+    #   NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
+    #   next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+    #   if next_page:
+    #      yield scrapy.Request(response.urljoin(next_page), callback = self.parse)
