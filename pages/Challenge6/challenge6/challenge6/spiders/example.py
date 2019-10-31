14c3c99f1347d0dd6a3647a459ffb7ed950d27d4
griffinstiens <griffinstiens@gmail.com>
2019-10-31 18:58:09 -0500

    scraper test

--- pages/Challenge6/challenge6/challenge6/spiders/example.py
+++ pages/Challenge6/challenge6/challenge6/spiders/example.py
@@ -1,5 +1,7 @@
 import scrapy
 
+import .. from items
+
 class ExampleSpider(scrapy.Spider):
    name = "brickset_spider"
    start_urls = ['http://brickset.com/sets/year-2018']
@@ -11,11 +13,12 @@ class ExampleSpider(scrapy.Spider):
          PIECES_SELECTOR = ".//dl[dt/text() = 'Pieces']/dd/a/text()"
          IMAGE_SELECTOR = "img ::attr(src)" 
 
-         yield {
-            'name': brickset.css(NAME_SELECTOR).extract_first(),
-            'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
-            'image': brickset.css(IMAGE_SELECTOR).extract_first()
-         }
+         item = items.Challenge6Item()
+         item['name'] = brickset.css(NAME_SELECTOR).extract_first(),
+         item['pieces'] = brickset.xpath(PIECES_SELECTOR).extract_first(),
+         item['image'] = brickset.css(IMAGE_SELECTOR).extract_first()
+
+         yield item
 
     #   NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
     #   next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
