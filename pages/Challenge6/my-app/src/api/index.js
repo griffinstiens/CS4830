2b5c3f03e5f67cdc5f3383a77a242b6340278da9
griffinstiens <griffinstiens@gmail.com>
2019-10-31 20:19:23 -0500

    scraper test

--- pages/Challenge6/my-app/src/api/index.js
+++ pages/Challenge6/my-app/src/api/index.js
@@ -4,12 +4,12 @@ const MongoClient = require('mongodb').MongoClient;
 
 let url = 'mongodb://localhost:27017/brickset';
 
-MongoClient.connect(url, (err, db) => {
+MongoClient.connect(url, (err, brickset) => {
 
     if (err) return console.log(err)
     console.log("Connection successful!");
 
-    db = client.db('brickset');
+    db = brickset.db('brickset');
 
     let cursor = db.collection('sets').find();
 
