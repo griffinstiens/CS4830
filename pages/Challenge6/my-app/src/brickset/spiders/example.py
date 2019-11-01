594eb5804eebcccc14221f526412c61496361c12
griffinstiens <griffinstiens@gmail.com>
2019-11-01 10:06:08 -0500

    test

---
+++ pages/Challenge6/my-app/src/brickset/spiders/example.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+#from brickset.items import BricksetItem
+from .. import items
+
+class BrickSetSpider(scrapy.Spider):
+    name = "brickset_spider"
+    start_urls = ['http://brickset.com/sets/year-2018']
+
+    def parse(self, response):
+        SET_SELECTOR = '.set'
+        for brickset in response.css(SET_SELECTOR): 
+            NAME_SELECTOR = "h1 ::text"
+            PIECES_SELECTOR = ".//dl[dt/text() = 'Pieces']/dd/a/text()"
+            IMAGE_SELECTOR = "img ::attr(src)" 
+            
+            item = items.BricksetItem()
+            
+            item['name'] = brickset.css(NAME_SELECTOR).extract_first()
+            item['pieces'] = brickset.xpath(PIECES_SELECTOR).extract_first()
+            item['image'] = brickset.css(IMAGE_SELECTOR).extract_first()
+
+            yield item
