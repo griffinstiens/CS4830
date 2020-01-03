b75f5b8869e5d9f6d87d5a72ebd0458d869d1145
griffinstiens <griffinstiens@gmail.com>
2020-01-03 12:29:07 -0600

    pushing portfolio

---
+++ pages/E-CommerceApp/precache-manifest.be8f184f9dac4efa62b683647fd17efa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f43ff3e25ae831f0e896ae7a4cf160a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad7773f70f966d270437",
+    "url": "./static/css/main.9b4ccad4.chunk.css"
+  },
+  {
+    "revision": "9f0de3df59d9eacc6701",
+    "url": "./static/js/2.1510dc3a.chunk.js"
+  },
+  {
+    "revision": "ad7773f70f966d270437",
+    "url": "./static/js/main.3db508ec.chunk.js"
+  },
+  {
+    "revision": "ffc8873a1d55aba703d1",
+    "url": "./static/js/runtime~main.fbcb963f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "./static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "./static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- pages/E-CommerceApp/service-worker.js
+++ pages/E-CommerceApp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pages/E-CommerceApp/precache-manifest.c40acdffafdb622f2520b96c1ef2be94.js"
+  "./precache-manifest.be8f184f9dac4efa62b683647fd17efa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pages/E-CommerceApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
